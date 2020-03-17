f772ddeb0adcad4e988ba3e6c7e607acdb91dce8
Deniz <vaisov96@mail.ru>
2020-03-17 19:23:18 +0300

    Updated SignalR invoke methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,18 @@
 <div id="text">
   <b-container fluid>
     <b-row>
-      <b-col md="9" class="mt-4"> 
-        <!-- <b-form-input type="number" v-model="fieldSize" placeholder="Размер поля" ></b-form-input> -->
+      <b-col offset-md="1" md="6" class="mt-4"> 
         <table id="gametable" class="table table-bordered table-responsive table-hover-cells">
           <tbody>
-            <tr v-for="row in field.rows" :key="row">
-              <td v-on:click="clickedCell(row, cell)" class="cell" v-for="cell in field.columns" :key="cell">
+            <tr v-for="row in gameField.length" :key="row">
+              <td v-on:click="clickedCell(row, cell)" class="cell" v-for="cell in gameField.length" :key="cell">
                 <!-- <img class="bomb" id="bomb" style="opacity: 0;" src="../assets/images/9-512.png"/> -->
               </td>
             </tr>
           </tbody>
         </table>
       </b-col>
-    </b-row>
-    <b-row>
-      <b-col offset-md="9" md="3">
+      <b-col class="mt-4" md="4">
         <HelloWorld msg="Чат" />
         <div class="mt-4" id="inputForm">
           <b-form-input v-model="user" type="text" placeholder="Логин" ></b-form-input>
@@ -35,9 +32,35 @@
                 </b-input-group-append>
           </b-input-group>
         </div>
+        <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 style="color: black;" class="text-center"></h3>
+        </div>
+        <div id="chatroom" class="mt-4 panel-body" style="max-height: 400px; overflow-y: scroll;">
+          <div v-for="(user, index) in listMessage" :item="user" :key="index" class="mb-4 max-w-sm mx-auto">
+            <div class="card">
+                <div class="contact-list">
+                    <div class="contact-list-item">
+                      <div class="contact-list-contact">
+                         <img src="" class="contact-list-avatar" alt=""> 
+                        <div>
+                          <div id="username" class="contact-list-name">
+                            {{ user.name }} 
+                          </div>
+                          <div id="usermessage" class="contact-list-email">
+                            {{ user.data }} 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
       </b-col>
       <b-col md="12">
-        <div v-for="(user, index) in listMessage" :item="user" :key="index" id="chatroom">
+        <!-- <div v-for="(user, index) in listMessage" :item="user" :key="index" id="chatroom">
           <b-col offset-md="2" md="8">
             <div class="mt-4 max-w-sm mx-auto">
             <div class="card">
@@ -59,7 +82,7 @@
               </div>
             </div>
           </b-col>
-        </div>
+        </div> -->
       </b-col>
     </b-row>
   </b-container>
@@ -84,11 +107,14 @@ export default {
     return {
       mine: "",
       field: {},
+      gameField: {},
       mines: [],
       postBody: {
         rows: "",
         cols: ""
       },
+      cell: "",
+      row: "",
       generatedMines: [],
       neighbourSquareNumbers: [],
       finished: false,
@@ -96,18 +122,30 @@ export default {
       user: "",
       message: "",
       listMessage: [],
-      connection: null
+      chatHubConnection: null,
+      gameHubConnection: null
     }
   },
   methods: {
     chat: function () {
-      if (this.connection.state === signalR.HubConnectionState.Connected) {
-        this.connection.invoke('SendMessage', this.user, this.message)
+      if (this.chatHubConnection.state === signalR.HubConnectionState.Connected) {
+        this.chatHubConnection.invoke('SendMessage', this.user, this.message)
       } else {
-        this.connection.start()
-        .then(() => this.connection.invoke('SendMessage', this.user, this.message))
+        this.chatHubConnection.start()
+        .then(() => this.chatHubConnection.invoke('SendMessage', this.user, this.message))
       }
     },
+
+    game: function() {
+      if(this.gameHubConnection.state === signalR.HubConnection.Connected) {
+       this.gameHubConnection.invoke('CellClick', this.row, this.cell)
+      } 
+      else {
+        this.gameHubConnection.start()
+        .then(() => this.gameHubConnection.invoke('CellClick', this.row, this.cell))
+      }
+    },
+
     clear: function() {
       this.listMessage = []
     },
@@ -118,99 +156,48 @@ export default {
       )
     },
 
-    clickedRow: function(row){
-      alert(row)
-    },
-
     clickedCell: function(row, cell) {
       let table = document.getElementById('gametable')
       table.rows[row-1].cells[cell-1].textContent = cell
       alert(row + '  ' + cell)
+
+      this.row = row - 1
+      this.cell = cell - 1
       
+      this.game()
+
       let postBody = {
           Columns: cell,
           Rows: row
       }
-	  
-      axios.post('https://localhost:5001/GameLogic/ClickedCell', postBody)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+      // this.generateMines(this.gameField, row, cell)
+    
+      // axios.post('https://localhost:5001/GameLogic/ClickedCell', postBody)
+      // .then(function (response) {
+      //   console.log(response);
+      // })
+      // .catch(function (error) {
+      //   console.log(error);
+      // });
+
     },
 
-    generateMines: function() {
-      let mines = this.mines
+    generateMines: function(row, cell) {
+      let postBody = {
+        clickedCell: this.gameField[row][cell].clickedCell = true
+      }
 
-
-      // for (let index = 0; index < this.fieldSize * this.fieldSize; index++) {
-      //   let randomCell = Math.ceil(Math.random() * this.fieldSize * this.fieldSize - 
-      //   Math.random() * this.fieldSize * this.fieldSize*2)
-      //   this.bombs.push(randomCell)
-      // }
+      // axios.post('https://localhost:5001/GameLogic/PlaceMines', postBody)
+      // .then(function (response) {
+      //   console.log(response);
+      // })
     },
 
     generateNeighbourBombsNumbers: function() {
-      let generatedBombs = this.generatedBombs
-      var cellNumbers = document.getElementsByTagName('td')
-      var rowNumbers = document.getElementsByTagName('tr')
-      var table = document.getElementById("gametable")
-      let minesCounter = 0
-      var bombs = document.getElementsByClassName('bomb')
-
-      let gridMatrix = []
-      for (let index = 0; index < this.fieldSize*this.fieldSize; index++) {
-        if(!generatedBombs.includes(index))
-          this.neighbourSquareNumbers.push(index)
-      }
-
-      for (let j = 0; j < this.fieldSize * this.fieldSize; j++) {
-          gridMatrix[j] = j
-      }
-
-      const chunkArray = (arr, cnt) => arr.reduce((prev, cur, i, a) => !(i % cnt) ? prev.concat([a.slice(i, i + cnt)]) : prev, []);
-
-      gridMatrix = chunkArray(gridMatrix, this.fieldSize)
-
-      for (var i = 0, row; row = table.rows[i]; i++) {
-        if (i < 0 || i >= row) continue;
-      //iterate through rows
-      //rows would be accessed using the "row" variable assigned in the for loop
-        for (var j = 0, col; col = row.cells[j]; j++) {
-          if (j < 0 || j >= col) continue;
-          //iterate through columns
-          //columns would be accessed using the "col" variable assigned in the for loop
-          if(bombs[j].style.cssText == 'opacity: 1;') {
-            
-          }
-        }  
-      }
-      // for (let index = 0; index < this.fieldSize*this.fieldSize; index++) {
-      //   cellNumbers[this.neighbourSquareNumbers[index]].textContent = this.neighbourSquareNumbers[index] + 1
-      // }
+      
     },
 
     displayBombs: function() {
-      // var bomb = document.createAttribute('data-bomb')
-      // bomb.value = "false"
-
-      var bombs = document.getElementsByClassName('bomb')
-      for (let index = 0; index < this.fieldSize * this.fieldSize; index++) {
-        if(this.bombs[index] <= 0){ 
-          continue
-        }
-
-        if(this.generatedBombs.includes(this.bombs[index])){
-          continue
-        }
-
-        this.generatedBombs.push(this.bombs[index])
-        let element = bombs[this.bombs[index]]
-        var imgStyle = element.style.cssText = 'opacity: 1;'
-      }
-
       this.generatedBombs.sort(function(a,b){
         return a - b
       })
@@ -218,30 +205,40 @@ export default {
   },
 
   created() {
-    
+    axios.get('https://localhost:5001/GameLogic/GameField').then((response) => 
+        this.gameField = response.data,
+      ).then((response) =>
+        console.log(response))
   },
   watch: {
     
   },
   mounted: function () {
-    this.generateField()
-
-    
+    // setInterval(() => this.generateField(), 10000);
     // this.generateBombs()
     // this.displayBombs()
     // this.generateNeighbourBombsNumbers()
-    
-    this.connection = new signalR.HubConnectionBuilder()
+    this.chatHubConnection = new signalR.HubConnectionBuilder()
       .withUrl('https://localhost:5001/chat')
       .configureLogging(signalR.LogLevel.Information)
       .build()
-      console.log("Connected", this.connection)
+      console.log("Connected", this.chatHubConnection)
 
-    this.connection.on('ReceiveMessage', (user, message) => {
+    this.gameHubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:5001/game')
+      .configureLogging(signalR.LogLevel.Information)
+      .build()
+      console.log("Connected", this.gameHubConnection)
+
+    this.chatHubConnection.on('ReceiveMessage', (user, message) => {
       const insertdata = {name: user, data: message}
       this.listMessage.push(insertdata)
       console.log(this.listMessage, 'listMessage')
     })
+
+    this.gameHubConnection.serverTimeoutInMilliseconds = 1000 * 60 * 10
+
+    this.gameHubConnection.on('CellClick', this.row, this.cell)
   }
 }
 </script>
